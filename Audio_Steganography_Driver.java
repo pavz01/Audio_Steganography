5cf488cbc9146eab6dc80aa8753e9ea3d26e0217
Cory Larcade <clarcade@gmail.com>
2014-05-28 22:05:05 -0400

    Added GUI and Event changes
    Added play and stop buttons. The play button has basic functionality.
    The stop button is hooked up for adding functional behavior. The play
    functionality was initially triggered when the "GO!" button was clicked,
    but the "GO!" button was actually for creating a new .wav file with the
    secret encrypt message. So I moved it to the play button's behavior.

--- Audio_Steganography_Driver.java
+++ Audio_Steganography_Driver.java
@@ -22,10 +22,11 @@ import javax.sound.sampled.*;
  */
 public class Audio_Steganography_Driver extends JPanel
                                         implements ActionListener {
-	JPanel encryptPanel, encryptSubPanel1, encryptSubPanel2, encryptSubPanel3;
-	JPanel decryptPanel, decryptSubPanel1, decryptSubPanel2, decryptSubPanel3;
-	JLabel encryptLabel, decryptLabel;
+	JPanel encryptPanel, encryptSubPanel1, encryptSubPanel2, encryptSubPanel3, encryptSubPanel4, encryptSubPanel5;
+	JPanel decryptPanel, decryptSubPanel1, decryptSubPanel2, decryptSubPanel3, decryptSubPanel4, decryptSubPanel5;
+	JLabel encryptLabel, decryptLabel, encryptPasswordLabel, decryptPasswordLabel;
 	JButton encryptFCButton, decryptFCButton, encryptRunButton, decryptRunButton;
+	JButton encryptPlayButton, encryptStopButton, decryptPlayButton, decryptStopButton;
 	JPasswordField encryptPassword, decryptPassword;
 	JTextArea encryptSecretMessage, decryptSecretMessage;
 	JFileChooser fc;
@@ -39,10 +40,14 @@ public class Audio_Steganography_Driver extends JPanel
 		encryptSubPanel1 = new JPanel(); // First subcontainer inside encryptPanel
 		encryptSubPanel2 = new JPanel(); // Second subcontainer inside encryptPanel
 		encryptSubPanel3 = new JPanel(); // Third subcontainer inside encryptPanel
+		encryptSubPanel4 = new JPanel(); // Fourth subcontainer inside encryptPanel
+		encryptSubPanel5 = new JPanel(); // Fifth subcontainer inside encryptPanel
 		decryptPanel = new JPanel();     // All decrypt content will be here
 		decryptSubPanel1 = new JPanel(); // First subcontainer inside decryptPanel
 		decryptSubPanel2 = new JPanel(); // Second subcontainer inside decryptPanel
 		decryptSubPanel3 = new JPanel(); // Third subcontainer inside decryptPanel
+		decryptSubPanel4 = new JPanel(); // Fourth subcontainer inside decryptPanel
+		decryptSubPanel5 = new JPanel(); // Fifth subcontainer inside decryptPanel
 
 		// Customize layout for all panels
 		encryptPanel.setLayout(new BoxLayout(encryptPanel, BoxLayout.PAGE_AXIS));
@@ -51,42 +56,58 @@ public class Audio_Steganography_Driver extends JPanel
 		/*
 		 * Encrypt GUI - Section
 		 */
-		// Create content for encryptSubPanel1
+		// Create "Encrypt" label
 		encryptLabel = new JLabel("Encrypt");
 		encryptLabel.setFont(new Font("Serif", Font.PLAIN, 14));
 		encryptLabel.setForeground(new Color(0x4198d9));
 
-		// Add sub-content to encryptSubPanel1
-		encryptSubPanel1.add(encryptLabel);
-
-		// Create content for encryptSubPanel2
+		// Create "Choose .WAV File" button
 		encryptFCButton = new JButton("<html><b>Choose a .WAV File</b></html>");
 		encryptFCButton.addActionListener(this);
 
+		// Create "Play" button
+		encryptPlayButton = new JButton("<html><b>Play</b></html>");
+		encryptPlayButton.addActionListener(this);
+		
+		// Create "Stop" button
+		encryptStopButton = new JButton("<html><b>Stop</b></html>");
+		encryptStopButton.addActionListener(this);
+		
+		// Create "Password" Label
+		encryptPasswordLabel = new JLabel("Enter Password:");
+		
+		// Create Password Field
 		encryptPassword = new JPasswordField(10); // password field
 
-		encryptRunButton = new JButton("<html><b>GO!</b></html>"); // GO! button
+		// Create "Make Secret Message WAV File" button
+		encryptRunButton = new JButton("<html><b>Make Secret Message WAV File</b></html>"); // GO! button
 		encryptRunButton.addActionListener(this);
 
-		// Add sub-content to encryptSubPanel2
-		encryptSubPanel2.add(encryptFCButton);
-		encryptSubPanel2.add(encryptPassword);
-		encryptSubPanel2.add(encryptRunButton);
-
-		// Create sub-content for encryptSubPanel3
-		// TO-DO -> CREATE A SOUND PLAYER HERE
+		// Create Secret Message text area
 		encryptSecretMessage = new JTextArea(6, 50); // Large enough for 300 characters to be displayed.
 		encryptSecretMessage.setLineWrap(true);
 		encryptSecretMessage.setWrapStyleWord(true);
 
-		// Add sub-content to encryptSubPanel3
-		// TO-DO -> ADD THE SOUND PLAYER HERE
-		encryptSubPanel3.add(encryptSecretMessage);
+		// Add content to sub-panels
+		encryptSubPanel1.add(encryptLabel);
 
-		// Add all encryptSubPanel's to encryptPanel
+		encryptSubPanel2.add(encryptFCButton);
+		
+		encryptSubPanel3.add(encryptPasswordLabel);
+		encryptSubPanel3.add(encryptPassword);
+		
+		encryptSubPanel4.add(encryptSecretMessage);
+		
+		encryptSubPanel5.add(encryptPlayButton);
+		encryptSubPanel5.add(encryptStopButton);
+		encryptSubPanel5.add(encryptRunButton);
+
+		// Add all encrypt sub-panels to encryptPanel
 		encryptPanel.add(encryptSubPanel1);
 		encryptPanel.add(encryptSubPanel2);
 		encryptPanel.add(encryptSubPanel3);
+		encryptPanel.add(encryptSubPanel4);
+		encryptPanel.add(encryptSubPanel5);
 
 		// Add encryptPanel to mainPanel
 		add(encryptPanel);
@@ -94,45 +115,60 @@ public class Audio_Steganography_Driver extends JPanel
 		/*
 		 * Decrypt GUI - Section
 		 */
-		// Create sub-content for decryptSubPanel1
+		// Create "Decrypt" label
 		decryptLabel = new JLabel("Decrypt");
 		decryptLabel.setFont(new Font("Serif", Font.PLAIN, 14));
 		decryptLabel.setForeground(new Color(0x4198d9));
 
-		// Add sub-content to decryptSubPanel1
-		decryptSubPanel1.add(decryptLabel); // "Decrypt" label
-
-		// Create sub-content for decryptSubPanel2
+		// Create "Choose .WAV File" button
 		decryptFCButton = new JButton("<html><b>Choose a .WAV File</b></html>");
 		decryptFCButton.addActionListener(this);
 
+		// Create "Play" button
+		decryptPlayButton = new JButton("<html><b>Play</b></html>");
+		decryptPlayButton.addActionListener(this);
+		
+		// Create "Stop" button
+		decryptStopButton = new JButton("<html><b>Stop</b></html>");
+		decryptStopButton.addActionListener(this);
+		
+		// Create "Password" Label
+		decryptPasswordLabel = new JLabel("Enter Password:");
+		
+		// Create Password Field
 		decryptPassword = new JPasswordField(10); // password field
 
-		decryptRunButton = new JButton("<html><b>GO!</b></html>"); // GO! button
+		// Create Secret Message WAV File
+		decryptRunButton = new JButton("<html><b>Decrypt Secret Message in WAV File</b></html>"); // GO! button
 		decryptRunButton.addActionListener(this);
 
-		// Add sub-content to decryptSubPanel2
-		decryptSubPanel2.add(decryptFCButton);
-		decryptSubPanel2.add(decryptPassword);
-		decryptSubPanel2.add(decryptRunButton);
-
-		// Create sub-content for decryptSubPanel3
-		// TO-DO -> CREATE A SOUND PLAYER HERE
-
+		// Display Area for Secret Message
 		decryptSecretMessage = new JTextArea(6, 50); // Large enough for 300 characters to be displayed.
 		decryptSecretMessage.setLineWrap(true);
 		decryptSecretMessage.setWrapStyleWord(true);
 
-		// Add sub-content to decryptSubPanel3
-		// TO-DO -> ADD SOUND PLAYER HERE
-		decryptSubPanel3.add(decryptSecretMessage);
+		// Add content to sub-panels
+		decryptSubPanel1.add(decryptLabel);
 
-		// Add all decryptSubPanel's to decryptPanel
+		decryptSubPanel2.add(decryptFCButton);
+		
+		decryptSubPanel3.add(decryptPasswordLabel);
+		decryptSubPanel3.add(decryptPassword);
+		
+		decryptSubPanel4.add(decryptSecretMessage);
+		
+		decryptSubPanel5.add(decryptPlayButton);
+		decryptSubPanel5.add(decryptStopButton);
+		decryptSubPanel5.add(decryptRunButton);
+
+		// Add all encrypt sub-panels to encryptPanel
 		decryptPanel.add(decryptSubPanel1);
 		decryptPanel.add(decryptSubPanel2);
 		decryptPanel.add(decryptSubPanel3);
+		decryptPanel.add(decryptSubPanel4);
+		decryptPanel.add(decryptSubPanel5);
 
-		// Add decryptPanel to mainPanel
+		// Add encryptPanel to mainPanel
 		add(decryptPanel);
 	}
 
@@ -174,6 +210,25 @@ public class Audio_Steganography_Driver extends JPanel
 			if (encryptFile != null){
 				System.out.println("Open the .wav file: " + encryptFile
 						+ ".\n Then encrypt message, and inject into new .wav file.");
+			}
+			else {
+				System.out.println("Encrypt Error: You must select a file first.");
+			}
+		}
+		
+		else if (eventSource == decryptRunButton) {
+			if (decryptFile != null){
+				System.out.println("Open the .wav file: " + decryptFile
+						+ ".\n Then decrypt message, and display the hidden message.");
+			}
+			else {
+				System.out.println("Decrypt Error: You must select a file first.");
+			}
+		}
+		
+		else if (eventSource == encryptPlayButton) {
+			if (encryptFile != null){
+				System.out.println("Play encrypt .wav file.");
 				try {
 				    AudioInputStream stream;
 				    AudioFormat format;
@@ -196,10 +251,43 @@ public class Audio_Steganography_Driver extends JPanel
 			}
 		}
 		
-		else if (eventSource == decryptRunButton) {
+		else if (eventSource == decryptPlayButton) {
 			if (decryptFile != null){
-				System.out.println("Open the .wav file: " + decryptFile
-						+ ".\n Then decrypt message, and display the hidden message.");
+				System.out.println("Play decrypt .wav file.");
+				try {
+				    AudioInputStream stream;
+				    AudioFormat format;
+				    DataLine.Info info;
+				    Clip clip;
+
+				    stream = AudioSystem.getAudioInputStream(decryptFile);
+				    format = stream.getFormat();
+				    info = new DataLine.Info(Clip.class, format);
+				    clip = (Clip) AudioSystem.getLine(info);
+				    clip.open(stream);
+				    clip.start();
+				}
+				catch (Exception e) {
+				    System.out.println("Error: " + e.getMessage());
+				}
+			}
+			else {
+				System.out.println("Decrypt Error: You must select a file first.");
+			}
+		}
+		
+		else if (eventSource == encryptStopButton) {
+			if (encryptFile != null){
+				System.out.println("Stop playing encrypt .wav file.");
+			}
+			else {
+				System.out.println("Encrypt Error: You must select a file first.");
+			}
+		}
+		
+		else if (eventSource == decryptStopButton) {
+			if (decryptFile != null){
+				System.out.println("Stop playing decrypt .wav file.");
 			}
 			else {
 				System.out.println("Decrypt Error: You must select a file first.");
