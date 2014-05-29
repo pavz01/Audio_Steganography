0fe0f94bc7e568a174aba7981ff063a597f1111f
Cory Larcade <clarcade@gmail.com>
2014-05-28 20:07:37 -0400

    Initial Refactor of GUI
    Started refactoring of GUI. Not yet aligned. Background colors given to
    help in aligning. Commented out old code, and am only working on encrypt
    portion of the GUI for now. Later will be the decrypt portion.

--- Audio_Steganography_Driver.java
+++ Audio_Steganography_Driver.java
@@ -22,10 +22,12 @@ import javax.sound.sampled.*;
  */
 public class Audio_Steganography_Driver extends JPanel
                                         implements ActionListener {
-	JPanel encryptPanel, encryptSubPanel1, encryptSubPanel2, encryptSubPanel3;
-	JPanel decryptPanel, decryptSubPanel1, decryptSubPanel2, decryptSubPanel3;
-	JLabel encryptLabel, decryptLabel;
+	JPanel encryptPanel, encryptSubPanel1, encryptSubPanel2; /*, encryptSubPanel3;*/
+	JPanel decryptPanel, decryptSubPanel1, decryptSubPanel2; /*, decryptSubPanel3;*/
+	JLabel encryptHeaderLabel, decryptHeaderLabel, passwordLabel;
+	JLabel encryptFileLabel, decryptFileLabel, secretMessageLabel;
 	JButton encryptFCButton, decryptFCButton, encryptRunButton, decryptRunButton;
+	JButton playButton, stopButton;
 	JPasswordField encryptPassword, decryptPassword;
 	JTextArea encryptSecretMessage, decryptSecretMessage;
 	JFileChooser fc;
@@ -33,7 +35,135 @@ public class Audio_Steganography_Driver extends JPanel
 
 	public Audio_Steganography_Driver() {
 		fc = new JFileChooser();
+
+		// Create all panels
+		encryptPanel = new JPanel();     // All encrypt content will be here
+		encryptSubPanel1 = new JPanel(); // First subcontainer inside encryptPanel
+		encryptSubPanel2 = new JPanel(); // Second subcontainer inside encryptPanel
+//		encryptSubPanel3 = new JPanel(); // Third subcontainer inside encryptPanel
+		decryptPanel = new JPanel();     // All decrypt content will be here
+		decryptSubPanel1 = new JPanel(); // First subcontainer inside decryptPanel
+		decryptSubPanel2 = new JPanel(); // Second subcontainer inside decryptPanel
+//		decryptSubPanel3 = new JPanel(); // Third subcontainer inside decryptPanel
+
+		// Customize layout for all panels
+		encryptPanel.setBackground(Color.GRAY);
 		
+		encryptSubPanel1.setLayout(new BoxLayout(encryptSubPanel1, BoxLayout.PAGE_AXIS));
+		encryptSubPanel1.setBackground(Color.BLUE);
+		
+		encryptSubPanel2.setBackground(Color.CYAN);
+		
+		decryptSubPanel1.setLayout(new BoxLayout(decryptSubPanel2, BoxLayout.PAGE_AXIS));
+
+		// ------ Encrypt GUI Section ------
+		// Create "Encrypt" label
+		encryptHeaderLabel = new JLabel("Encrypt");
+		encryptHeaderLabel.setFont(new Font("Serif", Font.PLAIN, 14));
+		encryptHeaderLabel.setForeground(new Color(0x4198d9));
+
+		// Create "Choose .WAV File" button
+		encryptFCButton = new JButton("<html><b>Choose .WAV File</b></html>");
+		encryptFCButton.addActionListener(this);
+//		encryptFCButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+		
+		// Create label for the name of the chosen file
+		encryptFileLabel = new JLabel("File:"); // Default to empty
+
+		// Create "Play" button
+		playButton = new JButton("<html><b>Play</b></html>");
+		playButton.addActionListener(this);
+		
+		// Create "Stop" button
+		stopButton = new JButton("<html><b>Stop</b></html>");
+		stopButton.addActionListener(this);
+		
+		// Create "Password:" label
+		passwordLabel = new JLabel("Enter Password");
+		
+		// Create Password Field
+		encryptPassword = new JPasswordField(10);
+		
+		// Create "Secret Message" label
+		secretMessageLabel = new JLabel("Secret Message");
+		
+		// Create Secret Message text area
+		encryptSecretMessage = new JTextArea(6, 50); // Large enough for 300 characters to be displayed.
+		encryptSecretMessage.setLineWrap(true);
+		encryptSecretMessage.setWrapStyleWord(true);
+
+		// Create Secret Message WAV File
+		encryptRunButton = new JButton("<html><b>Create Secret Message WAV File</b></html>"); // GO! button
+		encryptRunButton.addActionListener(this);
+
+		// Add content to encryptSubPanel2
+		encryptSubPanel2.add(playButton);
+		encryptSubPanel2.add(stopButton);
+
+		// Add content to encryptSubPanel1
+		encryptSubPanel1.add(encryptHeaderLabel);
+		encryptSubPanel1.add(encryptFCButton);
+		encryptSubPanel1.add(encryptFileLabel);
+		encryptSubPanel1.add(encryptSubPanel2);
+		encryptSubPanel1.add(passwordLabel);
+		encryptSubPanel1.add(encryptPassword);
+		encryptSubPanel1.add(secretMessageLabel);
+		encryptSubPanel1.add(encryptSecretMessage);
+		encryptSubPanel1.add(encryptRunButton);
+		
+		// Align content
+//		encryptHeaderLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		
+		// Add content to encryptPanel
+		encryptPanel.add(encryptSubPanel1);
+
+		// Add encryptPanel to main panel
+		add(encryptPanel);
+/*
+		// ------ Decrypt GUI - Section ------ NEW
+		// Create sub-content for decryptSubPanel1
+		decryptHeaderLabel = new JLabel("Decrypt");
+		decryptHeaderLabel.setFont(new Font("Serif", Font.PLAIN, 14));
+		decryptHeaderLabel.setForeground(new Color(0x4198d9));
+
+		// Add sub-content to decryptSubPanel1
+		decryptSubPanel1.add(decryptHeaderLabel); // "Decrypt" label
+
+		// Create sub-content for decryptSubPanel2
+		decryptFCButton = new JButton("<html><b>Choose a .WAV File</b></html>");
+		decryptFCButton.addActionListener(this);
+
+		decryptPassword = new JPasswordField(10); // password field
+
+		decryptRunButton = new JButton("<html><b>GO!</b></html>"); // GO! button
+		decryptRunButton.addActionListener(this);
+
+		// Add sub-content to decryptSubPanel2
+		decryptSubPanel2.add(decryptFCButton);
+		decryptSubPanel2.add(decryptPassword);
+		decryptSubPanel2.add(decryptRunButton);
+
+		// Create sub-content for decryptSubPanel3
+		// TO-DO -> CREATE A SOUND PLAYER HERE
+
+		decryptSecretMessage = new JTextArea(6, 50); // Large enough for 300 characters to be displayed.
+		decryptSecretMessage.setLineWrap(true);
+		decryptSecretMessage.setWrapStyleWord(true);
+
+		// Add sub-content to decryptSubPanel3
+		// TO-DO -> ADD SOUND PLAYER HERE
+		decryptSubPanel3.add(decryptSecretMessage);
+
+		// Add all decryptSubPanel's to decryptPanel
+		decryptPanel.add(decryptSubPanel1);
+		decryptPanel.add(decryptSubPanel2);
+		decryptPanel.add(decryptSubPanel3);
+
+		// Add decryptPanel to mainPanel
+		add(decryptPanel);
+*/
+		
+/*
 		// Create all panels
 		encryptPanel = new JPanel();     // All encrypt content will be here
 		encryptSubPanel1 = new JPanel(); // First subcontainer inside encryptPanel
@@ -48,9 +178,7 @@ public class Audio_Steganography_Driver extends JPanel
 		encryptPanel.setLayout(new BoxLayout(encryptPanel, BoxLayout.PAGE_AXIS));
 		decryptPanel.setLayout(new BoxLayout(decryptPanel, BoxLayout.PAGE_AXIS));
 
-		/*
-		 * Encrypt GUI - Section
-		 */
+		// ------ Encrypt GUI Section ------
 		// Create content for encryptSubPanel1
 		encryptLabel = new JLabel("Encrypt");
 		encryptLabel.setFont(new Font("Serif", Font.PLAIN, 14));
@@ -91,9 +219,7 @@ public class Audio_Steganography_Driver extends JPanel
 		// Add encryptPanel to mainPanel
 		add(encryptPanel);
 
-		/*
-		 * Decrypt GUI - Section
-		 */
+		// ------ Decrypt GUI - Section ------
 		// Create sub-content for decryptSubPanel1
 		decryptLabel = new JLabel("Decrypt");
 		decryptLabel.setFont(new Font("Serif", Font.PLAIN, 14));
@@ -134,6 +260,7 @@ public class Audio_Steganography_Driver extends JPanel
 
 		// Add decryptPanel to mainPanel
 		add(decryptPanel);
+*/
 	}
 
 	public void actionPerformed(ActionEvent event) {
