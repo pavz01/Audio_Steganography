b1f356945ab80b2d4b3b8913b6ecbc61e55c278f
Cory Larcade <clarcade@gmail.com>
2014-06-22 00:29:25 -0400

    Added Functionality to Classes & Function
    Finished providing functionality to the following classes: WavHeader,
    WavFormatChunk, WavDataChunk. Finished providing functionality to
    injectEncryptedMessage(). Added debug test code and a setter for JWav's
    message variable. Removed one parameter from encryptMessage function.

--- JWav.java
+++ JWav.java
@@ -33,9 +33,9 @@ import javax.sound.sampled.*;
 public class JWav {
 	// ------ MEMBER VARIABLES ------
 	private JFileChooser fc;
-	private WavHeader wavHeader;
-	private WavFormatChunk wavFormat;
-	private WavDataChunk wavData;
+	private WavHeader wavHeaderChunk;
+	private WavFormatChunk wavFormatChunk;
+	private WavDataChunk wavDataChunk;
 	private File file;
 	private String message;
 	private byte[] allWavData;
@@ -104,9 +104,15 @@ public class JWav {
 
 	// ------ SETTERS ------
 	// Function: setWavFileToNotValid
-	// Description: A setter function to the "validWavFile" to false
+	// Description: A setter function to set "validWavFile" to false
 	public void setWavFileToNotValid() {
 		validWavFile = false;
+	}
+	
+	// Function: setMessage
+	// Description: A setter function to set "message"
+	public void setMessage (String guiMessage) {
+		message = guiMessage;
 	}
 	
 	// ------ GETTERS ------
@@ -260,14 +266,6 @@ public class JWav {
 		
 		// Read in the WAV file data
 		readWavFileData();
-		
-		// Break the WAV's data into it's corresponding parts:
-		//      Header, Format, Data.
-		// Will be useful later for manipulating the data and injecting it into
-		// a new WAV file.
-//		wavHeader = new WavHeader();
-//		wavFormat = new WavFormatChunk(); // TO-DO
-//		wavData = new WavDataChunk(); // TO-DO
 	}
 	
 	// Function: initialize
@@ -297,7 +295,7 @@ public class JWav {
 	// Description: Will use the user-provided password and encrypt the
 	//              user-provided message.
 	// TO-DO
-	public void encryptMessage (char[] guiPassword, String guiMessage) {
+	public void encryptMessage (char[] guiPassword) {
 		System.out.println("Called encryptMessage function.");
 /*
 //		System.out.println("encryptMessage: Password = " + guiPassword.toString());
@@ -313,6 +311,7 @@ public class JWav {
 	public void injectEncryptedMessage () throws Exception {
 		System.out.println("Called injectEncryptedMessage function.");
 		
+		/*
 		// TEST #1 START
 		// Status: Passed. :)
 		// Let's assume we have been able to convert our message from the GUI into a byte array
@@ -349,13 +348,165 @@ public class JWav {
 		fop.write(tempAllData);
 		fop.close();
 		// TEST #1 END
+		*/
 		
 		// TEST #2 START - Simulate message.
-		// Status: TO-DO
-		// Now we're going to convert a String message into bits, insert it into
-		// the byte array, and finally write to the WAV file using the byte array.
-		//message = "Hello World!";
+		// Status: PASSED. :)
+		// We are going to test out our binary manipulation now.
+		/*
+		byte byteTestZero = (byte)0x63;
+		byte zeroMask = (byte)0xfe; // Need to use & operator
+		byte byteTestOne = (byte)0x58;
+		byte oneMask = (byte)0x01; // Need to use | operator
+		
+		System.out.println("Should be 98: " + (byteTestZero & zeroMask));
+		System.out.println("Should be 89: " + (byteTestOne | oneMask));
+		
 		// TEST #2 END
+		*/
+		
+		// TEST #3 START - Simulate message.
+		// Status: Passed. :)
+		// Now we're going to convert a String message into a byte array, 
+		// and write to the WAV file using the byte array.
+		/*
+		int numCharacters = message.length();
+		byte[] bytesToHide = message.getBytes();
+		byte maskByte = (byte) 0x80;
+		int dataSize = allWavData.length - 44;
+		int sizeNeeded = 64 * numCharacters;
+		byte maskZero = (byte)0xfe; // Need to use & operator
+		byte maskOne = (byte)0x01; // Need to use | operator
+		byte maskReset = (byte)0x80;
+		
+		if (dataSize < sizeNeeded) {
+			throw new Exception ("Error: WAV file is too small to hide message.");
+		}
+
+		// Inject all characters of the message into allWavData
+		// BUG ALERT: We're currently injecting our bytes into each byte sequentially.
+		//   This can change an int of: 30452 or 0111 0110 1111 0100
+		//                          to: 30709 or 0111 0111 1111 0101.
+		//   That's why we're getting tons of static initially.
+		for (int i = 0, j = 100000; i < numCharacters; j++) {
+			// Determine if the current bit for the current character is a one
+			if ( (bytesToHide[i] & maskByte) == maskByte) {
+				// Mask the LSB of the current byte of allWavData with a one
+				allWavData[j] = (byte)(allWavData[j] | maskOne);
+			}
+			// If it's not a one, then it's a zero
+			else {
+				// Mask the LSB of the current byte of allWavData with a zero
+				allWavData[j] = (byte)(allWavData[j] & maskZero);
+			}
+
+			// Check if we have injected the last bit of the current character
+			if (maskByte == maskOne) {
+				// If so, reset the bit to check for to the MSB
+				maskByte = maskReset;
+				
+				// Then, move on to the next character to inject
+				i++;
+			}
+			// Otherwise, move on to the next bit of the current character
+			else {
+				maskByte = (byte)((maskByte & 0xff) >>> 1);
+			}
+		}
+		
+		FileOutputStream fop = new FileOutputStream(file);
+		fop.write(allWavData);
+		fop.close();
+		
+		System.out.println("test3");
+		// TEST #3 END
+		*/
+		
+		/*
+		// TEST 4 START - Initialize WavHeader, WavFormatChunk, WavDataChunk.
+		// Status: Passed. :)
+		// Break the WAV's data into it's corresponding parts: Header, Format, Data.
+		// Will be useful later for manipulating the data and injecting it into
+		// a new WAV file.
+		wavHeader = new WavHeader(); // DONE
+		wavHeader.display();
+		wavFormat = new WavFormatChunk(); // DONE
+		wavFormat.display();
+		wavData = new WavDataChunk(); // DONE
+		wavData.display();
+		
+		System.out.println("test4");
+		// TEST #4 END
+		*/
+		
+		// TEST 5 START - Use the new classes to modify and update the WAV file.
+		// Upon completion of this test, the user will not be able to notice
+		// the difference between the original WAV file and the WAV file that
+		// has the hidden message contained in it. This should be the last step
+		// for completing the main functionality for this function.
+		// Status: Passed. :)
+		wavHeaderChunk = new WavHeader();
+		wavFormatChunk = new WavFormatChunk();
+		wavDataChunk = new WavDataChunk();
+		
+		int numCharacters = message.length();
+		byte[] bytesToHide = message.getBytes();
+		byte maskByte = (byte) 0x80;
+		byte[] tempData = wavDataChunk.getData();
+		int dataSize = tempData.length;
+		int byteOffset = wavFormatChunk.getBlockAlign();
+		int sizeNeeded = 64 * numCharacters * byteOffset;
+		byte maskZero = (byte)0xfe; // Need to use & operator
+		byte maskOne = (byte)0x01; // Need to use | operator
+		byte maskReset = (byte)0x80;
+		
+		if (dataSize < sizeNeeded) {
+			throw new Exception ("Error: WAV file is too small to hide message.");
+		}
+
+		// Inject all characters of the message into allWavData
+		for (int i = 0, j = 0; i < numCharacters; j = j + byteOffset) {
+			// Determine if the current bit for the current character is a one
+			if ( (bytesToHide[i] & maskByte) == maskByte) {
+				// Mask the LSB of the current byte of allWavData with a one
+				tempData[j] = (byte)(tempData[j] | maskOne);
+			}
+			// If it's not a one, then it's a zero
+			else {
+				// Mask the LSB of the current byte of allWavData with a zero
+				tempData[j] = (byte)(tempData[j] & maskZero);
+			}
+
+			// Check if we have injected the last bit of the current character
+			if (maskByte == maskOne) {
+				// If so, reset the bit to check for to the MSB
+				maskByte = maskReset;
+				
+				// Then, move on to the next character to inject
+				i++;
+			}
+			// Otherwise, move on to the next bit of the current character
+			else {
+				maskByte = (byte)((maskByte & 0xff) >>> 1);
+			}
+		}
+		
+		// Copy tempData to allWavData
+		if (tempData.length == (allWavData.length - 44)) {
+			for (int i = 0; i < dataSize; i++) {
+//				System.out.print("allWavData[" + (i + 4) + "] before is: " + allWavData[i + 44]);
+				allWavData[i + 44] = tempData[i];
+//				System.out.println("         allWavData[" + (i + 44) + "] after is: " + allWavData[i + 44]);
+			}
+		}
+		
+		// Overwrite the WAV file with new data that contains the hidden message
+		FileOutputStream fop = new FileOutputStream(file);
+		fop.write(allWavData);
+		fop.close();
+		
+		System.out.println("test5");
+		// TEST #5 END
 	}
 
 	// Function: decryptMessage
@@ -436,38 +587,39 @@ public class JWav {
 	// Description: Will contain the WAV file's header info.
 	private class WavHeader {
 		// ------ MEMBER VARIABLES ------
-		private byte[] chunkID; // Should be "RIFF"
+		private String chunkID; // Should be "RIFF"
 		private Integer chunkSize;
-		private byte[] format;
+		private String format; // Should be "WAVE"
 		
 		// ------ CONSTRUCTORS ------
 		// Constructor: WavHeader()
 		public WavHeader () {
 			try {
-				// Grab the chunkID. Should be "RIFF"
 				System.out.println("WavHeader Constructor");
-				chunkID = new byte[4];
+				
+				// Set chunkID
+				byte[] tempByteArray = new byte[4];
 				for (int i = 0; i < 4; i++) {
-					chunkID[i] = allWavData[i];
+					tempByteArray[i] = allWavData[i];
 				}
-				System.out.println("chunkID: " + new String(chunkID, "UTF-8"));
+				chunkID = new String(tempByteArray, "UTF-8");
 
 				// Grab the size of the WAV file
-				byte[] tempSize = new byte[4];
+				tempByteArray = new byte[4];
 				for (int i = 0; i < 4; i++) {
-					tempSize[i] = allWavData[i + 4];
+					tempByteArray[i] = allWavData[i + 4];
 				}
-				ByteBuffer bb = ByteBuffer.wrap(tempSize);
+				ByteBuffer bb = ByteBuffer.wrap(tempByteArray);
 				bb.order(ByteOrder.LITTLE_ENDIAN);
 				chunkSize = bb.getInt();
-				System.out.println("chunkSize: " + chunkSize);
 				
 				// Grab the format of the file. Should be "WAVE"
-				format = new byte[4];
+				tempByteArray = new byte[4];
 				for (int i = 0; i < 4; i++) {
-					format[i] = allWavData[i + 8];
+					tempByteArray[i] = allWavData[i + 8];
 				}
-				System.out.println("format: " + new String(format, "UTF-8"));
+				format = new String(tempByteArray, "UTF-8");
+
 			}
 			catch (Exception e) {
 				System.out.println("WavHeader Error: " + e.getMessage());
@@ -477,9 +629,10 @@ public class JWav {
 		public void display () {
 			try {
 				System.out.println("HEADER CHUNK CONTENTS:");
-				System.out.println("chunkID: " + new String(chunkID, "UTF-8"));
-				System.out.println("chunkSize: " + chunkSize);
-				System.out.println("format: " + new String(format, "UTF-8"));
+				System.out.println("chunkID is: " + chunkID);
+				System.out.println("chunkSize is: " + chunkSize);
+				System.out.println("chunkSize should be: " + (allWavData.length - 8));
+				System.out.println("format is: " + format);
 			}
 			catch (Exception e) {
 				System.out.println("WavHeader.display Error: " + e.getMessage());
@@ -491,13 +644,116 @@ public class JWav {
 	// Description: Will contain the WAV file's format info.
 	private class WavFormatChunk {
 		// ------ MEMBER VARIABLES ------
-		// TO-DO
+		private String chunkID; // Contains the letters "fmt "
+		private Integer chunkSize; // size of the rest of the sub-chunk. 16 for PCM.
+		private Integer audioFormat; // values other than 1 indicate some form of compression
+		private Integer numChannels; // Mono = 1, Stereo = 2, etc.
+		private Integer sampleRate;
+		private Integer byteRate;
+		private Integer blockAlign; // # of bytes for 1 sample
+		private Integer bitsPerSample;
 		
 		// ------ CONSTRUCTORS ------
 		// Constructor: WavFormatChunk()
-		// TO-DO
 		public WavFormatChunk () {
 			System.out.println("WavFormatChunk Constructor");
+			
+			try {
+				// Set chunkID
+				byte[] tempByteArray = new byte[4];
+				for (int i = 0; i < 4; i++) {
+					tempByteArray[i] = allWavData[i + 12];
+				}
+				chunkID = new String(tempByteArray, "UTF-8");
+
+				// Set chunkSize
+				tempByteArray = new byte[4];
+				for (int i = 0; i < 4; i++) {
+					tempByteArray[i] = allWavData[i + 16];
+				}
+				ByteBuffer bb = ByteBuffer.wrap(tempByteArray);
+				bb.order(ByteOrder.LITTLE_ENDIAN);
+				chunkSize = bb.getInt();
+				
+				// Set audioFormat
+				tempByteArray = new byte[4];
+				for (int i = 0; i < 2; i++) {
+					tempByteArray[i] = allWavData[i + 20];
+				}
+				bb = ByteBuffer.wrap(tempByteArray);
+				bb.order(ByteOrder.LITTLE_ENDIAN);
+				audioFormat = bb.getInt();
+				
+				// Set numChannels
+				tempByteArray = new byte[4];
+				for (int i = 0; i < 2; i++) {
+					tempByteArray[i] = allWavData[i + 22];
+				}
+				bb = ByteBuffer.wrap(tempByteArray);
+				bb.order(ByteOrder.LITTLE_ENDIAN);
+				numChannels = bb.getInt();
+				
+				// Set sampleRate
+				tempByteArray = new byte[4];
+				for (int i = 0; i < 4; i++) {
+					tempByteArray[i] = allWavData[i + 24];
+				}
+				bb = ByteBuffer.wrap(tempByteArray);
+				bb.order(ByteOrder.LITTLE_ENDIAN);
+				sampleRate = bb.getInt();
+				
+				// Set byteRate
+				tempByteArray = new byte[4];
+				for (int i = 0; i < 4; i++) {
+					tempByteArray[i] = allWavData[i + 28];
+				}
+				bb = ByteBuffer.wrap(tempByteArray);
+				bb.order(ByteOrder.LITTLE_ENDIAN);
+				byteRate = bb.getInt();
+				
+				// Set blockAlign
+				tempByteArray = new byte[4];
+				for (int i = 0; i < 2; i++) {
+					tempByteArray[i] = allWavData[i + 32];
+				}
+				bb = ByteBuffer.wrap(tempByteArray);
+				bb.order(ByteOrder.LITTLE_ENDIAN);
+				blockAlign = bb.getInt();
+				
+				// Set bitsPerSample
+				tempByteArray = new byte[4];
+				for (int i = 0; i < 2; i++) {
+					tempByteArray[i] = allWavData[i + 34];
+				}
+				bb = ByteBuffer.wrap(tempByteArray);
+				bb.order(ByteOrder.LITTLE_ENDIAN);
+				bitsPerSample = bb.getInt();
+			}
+			catch (Exception e) {
+				System.out.println("Error WavFormatChunk: " + e.getMessage());
+			}
+		}
+		
+		public Integer getBlockAlign () {
+			return blockAlign;
+		}
+		
+		public void display () {
+			try {
+				System.out.println("FORMAT CHUNK CONTENTS:");
+				System.out.println("chunkID is: " + chunkID);
+				System.out.println("chunkSize is: " + chunkSize);
+				System.out.println("audioFormat is: " + audioFormat);
+				System.out.println("numChannels is: " + numChannels);
+				System.out.println("sampleRate is: " + sampleRate);
+				System.out.println("byteRate is: " + byteRate);
+				System.out.println("byteRate should be: " + (sampleRate * numChannels * bitsPerSample / 8));
+				System.out.println("blockAlign is: " + blockAlign);
+				System.out.println("bitsPerSample is: " + bitsPerSample);
+			}
+			catch (Exception e) {
+				System.out.println("WavFormatChunk.display Error: " + e.getMessage());
+			}
 		}
 	}
 	
@@ -505,13 +761,72 @@ public class JWav {
 	// Description: Will contain the WAV file's data info.
 	private class WavDataChunk {
 		// ------ MEMBER VARIABLES ------
-		// TO-DO
+		private String chunkID;
+		private Integer chunkSize;
+		private byte[] data;
 		
 		// ------ CONSTRUCTORS ------
 		// Constructor: WavDataChunk()
-		// TO-DO
 		public WavDataChunk () {
 			System.out.println("WavDataChunk Constructor");
+			
+			try {
+				// Set chunkID
+				byte[] tempByteArray = new byte[4];
+				for (int i = 0; i < 4; i++) {
+					tempByteArray[i] = allWavData[i + 36];
+				}
+				chunkID = new String(tempByteArray, "UTF-8");
+
+				// Set chunkSize
+				tempByteArray = new byte[4];
+				for (int i = 0; i < 4; i++) {
+					tempByteArray[i] = allWavData[i + 40];
+				}
+				ByteBuffer bb = ByteBuffer.wrap(tempByteArray);
+				bb.order(ByteOrder.LITTLE_ENDIAN);
+				chunkSize = bb.getInt();
+				
+				// Make sure chunkSize is right
+				if (chunkSize != (allWavData.length - 44)) {
+					throw new Exception("Aaaaah! chunkSize isn't what it should be!");
+				}
+				
+				// Set data
+				data = new byte[chunkSize];
+				for (int i = 0; i < chunkSize; i++) {
+					data[i] = allWavData[i + 44];
+				}
+			}
+			catch (Exception e) {
+				System.out.println("WavData Chunk Error: " + e.getMessage());
+			}
+		}
+		
+		public byte[] getData() {
+			return data;
+		}
+		
+		public void display () {
+			try {
+				System.out.println("DATA CHUNK CONTENTS:");
+				System.out.println("chunkID is: " + chunkID);
+				System.out.println("chunkSize is: " + chunkSize);
+				System.out.println("chunkSize should be: " + (allWavData.length - 44));
+				
+				/*  // For debug purposes. Both outputs should be exactly the same.
+				// Compare all elements of both arrays.
+//				for (int i = 0, j = 44; i < data.length; i++, j++) { 
+				// Compare the last 1000000 elements of both arrays
+				for (int i = data.length - 1000000, j = allWavData.length - 1000000; i < data.length; i++, j++) {
+					System.out.print("data[" + i + "] is: " + data[i]);
+					System.out.println("         allWavData[" + j + "] is: " + allWavData[j]);
+				}
+				*/
+			}
+			catch (Exception e) {
+				System.out.println("WavDataChunk.display Error: " + e.getMessage());
+			}
 		}
 	}
 }
